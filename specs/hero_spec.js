3fe540885c34fd0752b65262365d85c88a57bc1f
NickBulbeck <nickbulbeck@btinternet.com>
2017-06-16 13:48:12 +0100

    Set up initial Hero spec

---
+++ specs/hero_spec.js
@@ -0,0 +1,44 @@
+var Hero = require("../hero");
+// var Task = require("../task");
+// var Food = require("../food");
+
+describe("Nick's D&D hamework...",function(){
+  it("ONE: Hero has a name", function(){
+
+  });
+  // it("TWO: Hero has a health-level", function(){
+
+  // });
+  // it("THREE: Hero has a favourite food", function(){
+
+  // });
+  // it("FOUR: Hero can talk", function(){
+
+  // });
+  // it("FIVE: Hero has a mission ", function(){
+
+  // });
+  // it("SIX: Hero can eat", function(){
+
+  // })
+  // it("SEVEN: Hero can spot favourite food", function(){
+
+  // })
+  // it("EIGHT: Hero can sort task-list by difficulty", function(){
+    
+  // })
+  // it("NINE: Hero can sort task-list by urgency", function(){
+    
+  // })
+  // it("TEN: Hero can sort task-list by reward", function(){
+    
+  // })
+  // it("ELEVEN: Hero can view completed tasks", function(){
+    
+  // })
+  // it("TWELVE: Hero can view incomplete tasks", function(){
+    
+  // })
+
+
+})
