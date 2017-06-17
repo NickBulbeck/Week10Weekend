66e79c947cd52df1a588ca996297a45e08707fc4
NickBulbeck <nickbulbeck@btinternet.com>
2017-06-17 09:29:41 +0100

    Food has a nutritional value: pass

--- specs/food_spec.js
+++ specs/food_spec.js
@@ -8,10 +8,10 @@ describe("Nick's D&D hamework...",function(){
     var lasagne = new Food("lasagne",7);
     assert.equal("lasagne",lasagne.name);
   });
-  // it("2: Food has a nutritional value", function(){
-  //   var lasagne = new Food("lasagne",7);
-  //   // assert.equal(7,lasagne.nutrition);
-  // });
+  it("2: Food has a nutritional value", function(){
+    var lasagne = new Food("lasagne",7);
+    assert.equal(7,lasagne.nutrition);
+  });
   // it("2: Food starts uncontaminated", function(){
   //   var lasagne = new Food("lasagne",7);
   //   // assert.equal(false,lasagne.isContaminated);
