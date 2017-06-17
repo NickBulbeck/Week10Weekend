8e1afa3c460c558ee94e9c3e7251b4b48bc0cf51
NickBulbeck <nickbulbeck@btinternet.com>
2017-06-17 09:15:27 +0100

    Complete initial set of task tests

--- specs/task_spec.js
+++ specs/task_spec.js
@@ -8,6 +8,24 @@ describe("Nick's D&D hamework...",function(){
     // var task1 = new Task();
     // assert.equal(10,task1.difficulty);
   });
+  // it("1: Task has an urgency level", function(){
+    // var task1 = new Task();
+    // assert.equal(5,task1.urgency);
+  // });
+  // it("1: Task has a reward level", function(){
+    // var task1 = new Task();
+    // assert.equal(2,task1.reward);
+  // });
+  // it("1: Task begins uncompleted", function(){
+    // var task1 = new Task();
+    // assert.equal(false,task1.isComplete);
+  // });
+  // it("1: Task can be marked as complete", function(){
+    // var task1 = new Task();
+    // assert.equal(false,task1.isComplete);
+    // task1.markAsComplete();
+    // assert.equal(true,task1.isComplete);
+  // });
 
 
 
