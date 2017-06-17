6b2c9ae20d7625720b7056803385c2a1485c20e4
NickBulbeck <nickbulbeck@btinternet.com>
2017-06-17 09:06:36 +0100

    Insert skeleton code into task files

---
+++ specs/task_spec.js
@@ -0,0 +1,19 @@
+var Hero = require("../hero");
+var Task = require("../task");
+// var Food = require("../food");
+var assert = require('assert');
+
+describe("Nick's D&D hamework...",function(){
+  it("1: Task has a difficulty level", function(){
+    // var task1 = new Task();
+    // assert.equal(10,task1.difficulty);
+  });
+
+
+
+
+
+
+
+
+}
---
+++ task.js
@@ -0,0 +1,10 @@
+var _ = require("lodash");
+
+var Task = function(name) {
+  this.name = name;
+}
+
+Task.prototype = {
+
+}
+module.exports = Task;
